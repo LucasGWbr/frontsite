a3b410a907924d0b9dbd3c18f4416c9739ad809e
Lucas Gustavo Weber <lucas.weber1@universo.univates.br>
2025-11-12 15:19:15 -0300

    abrir acesso externo

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permite conexões externas
+    port: 5173
+  }
 })
